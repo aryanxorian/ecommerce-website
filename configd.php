335994b5d8d06f905005c94d345d9b761b71237a
Sushanta Mahato <sushantam@mindfiresolutions.com>
2021-03-25 17:46:08 +0530

    configd updated

--- configd.php
+++ configd.php
@@ -3,4 +3,9 @@
     $dbname = 'E-commerce';
     $username = 'subhajit';
     $dbpassword = 'nahipata1234#';
+    $conn = new mysqli($host, $username, $dbpassword, $dbname);
+    if ($conn->connect_error) 
+    {
+        die("Connection failed: " . $conn->connect_error);
+    }
 ?>
